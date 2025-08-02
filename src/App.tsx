54604a94be5a29a476d6c6f92b3bf0540f2cf75e
sam@ketl.ch <sam@ketl.ch>
2025-08-02 18:57:05 +0200

    router-and-i18

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import arkFidLogo from "./assets/arkfid--color.svg";
-import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About"; 
+
 
 function App() {
   return (
-    <>
-      <div>
-        <div style={{ marginRight: "auto", marginLeft: "auto", width: 100, marginBottom: 100, marginTop: 100 }}>
-          <img src={arkFidLogo} width={100} />
-        </div>
-        <b>We'll Be Back Soon!</b>
-        <p>
-          Regrettably, this website is temporarily unavailable as we perform
-          necessary maintenance.
-        </p>
-      </div>
-    </>
+    <div>
+      {/* You can have a shared navigation here */}
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
