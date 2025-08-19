48bdc0003aa27d2a00cf97109122642882218764
sam@ketl.ch <sam@ketl.ch>
2025-08-19 23:26:11 +0200

    base

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  server: {
+    port: 3000,
+  },
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 });
