060551c13d33c0da7dd15a406b89efbed870ca99
Sam <samuelhalff@gmail.com>
2025-08-22 09:52:42 +0000

    cleanup

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,37 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/Contact/ContactForm",
+    "/Home/Home",
+    "/Home/components/hero",
+    "/Home/components/testimonials",
+    "/Home/faq",
+    "/Services/Accounting/Accounting",
+    "/Services/Accounting/components/hero",
+    "/Services/Accounting/components/presentation",
+    "/Services/Corporate/Corporate",
+    "/Services/Corporate/components/hero",
+    "/Services/Corporate/components/presentation",
+    "/Services/Domiciliation/components/presentation",
+    "/Services/Odoo/Odoo",
+    "/Services/Odoo/components/hero",
+    "/Services/Odoo/components/presentation",
+    "/Services/Outsourcing/Outsourcing",
+    "/Services/Outsourcing/components/hero",
+    "/Services/Outsourcing/components/presentation",
+    "/Services/Payroll/Payroll",
+    "/Services/Payroll/components/hero",
+    "/Services/Payroll/components/presentation",
+    "/Services/Taxes/Taxes",
+    "/Services/Taxes/components/hero",
+    "/Services/Taxes/components/presentation",
+    "/Services/services",
+    "/_app",
+    "/_error",
+    "/shared/footer"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
