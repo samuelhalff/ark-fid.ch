802875594ecd20ba8408bab8548ed4d4bd8d4f1a
Sam <samuelhalff@gmail.com>
2025-09-28 13:04:30 +0000

    fi

--- app/[locale]/home/components/hero.tsx
+++ app/[locale]/home/components/hero.tsx
@@ -82,7 +82,7 @@ const Hero = async ({ locale }: HeroProps) => {
         <div className="relative lg:max-w-lg xl:max-w-xl w-full bg-accent rounded-xl aspect-square motion-safe:lg:animate-in motion-safe:lg:slide-in-from-right-10 lg:duration-500 critical-hero__media">
           <Image
             src={homeHeroSrc}
-            alt="featured service"
+            alt="Services professionnels de comptabilité et fiscalité offerts par Ark Fiduciaire"
             className="object-cover rounded-xl"
             sizes="(min-width:1280px) 560px, (min-width:1024px) 480px, 92vw"
             quality={72}
